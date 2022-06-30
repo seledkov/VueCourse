4fc1b28eb606990f690144cdfaf39b1f0fe6ccea
unknown <seledkovti@gmail.com>
2022-07-01 01:44:30 +0300

    events building

--- 02_Core_Concept/app.js
+++ 02_Core_Concept/app.js
@@ -24,21 +24,29 @@ Vue.createApp({
     return {
       counter: 10,
       name: '',
+      lastName: '',
       title: '',
     };
   },
   computed: {
     fullName() {
       // work as returned data but foo
-      return this.name + ' ' + 'miller';
+      return this.name + ' ' + this.lastName;
     },
   },
   watch: {
-    name(newValue, oldValue) {
-      // see to key into data > name(gdata.name)
-      console.log('hi i am watch: name', newValue, oldValue);
-      this.title = newValue + ' fon miller';
+    counter(currentValue) {
+      if (currentValue < -10) {
+        setTimeout(() => {
+          this.counter = 0;
+        }, 1000);
+      }
     },
+    // name(newValue, oldValue) {
+    //   // see to key into data > name(gdata.name)
+    //   console.log('hi i am watch: name', newValue, oldValue);
+    //   this.title = newValue + ' fon miller';
+    // },
   },
   methods: {
     console(event) {
@@ -61,3 +69,33 @@ Vue.createApp({
     },
   },
 }).mount('#events');
+
+Vue.createApp({
+  data() {
+    return { counter: 0, winValue: 21 };
+  },
+  watch: {
+    counter() {
+      setTimeout(() => {
+        this.counter = 0;
+      }, 4000);
+    },
+  },
+  computed: {
+    resultMsg() {
+      if (this.counter < this.winValue) {
+        return this.counter + ' low';
+      } else if (this.counter > this.winValue) {
+        return this.counter + ' over';
+      } else {
+        return 'you won';
+      }
+    },
+  },
+  methods: {
+    add(num) {
+      console.log(this.counter);
+      this.counter += num;
+    },
+  },
+}).mount('#event-binding');

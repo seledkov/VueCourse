6d1b6878f6d1ca2a8fdc799784d02ac86566262f
unknown <seledkovti@gmail.com>
2022-07-10 14:03:32 +0300

    v111 use <slot/> into example wrapper component

--- 09_Diving_Deeper_into_Components/src/main.js
+++ 09_Diving_Deeper_into_Components/src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 // import TheHeader from './components/TheHeader.vue';
 import BaseBadge from './components/BaseBadge.vue';
+import BaseCard from './components/BaseCard.vue';
 // import BadgeList from './components/BadgeList.vue';
 // import UserInfo from './components/UserInfo.vue';
 
@@ -10,6 +11,7 @@ const app = createApp(App);
 
 // app.component('the-header', TheHeader);
 app.component('base-badge', BaseBadge);
+app.component('base-card', BaseCard);
 // app.component('badge-list', BadgeList);
 // app.component('user-info', UserInfo);
 

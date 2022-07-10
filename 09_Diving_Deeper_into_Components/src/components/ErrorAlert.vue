856775811841a9773107894e20e7027df3d3e83a
unknown <seledkovti@gmail.com>
2022-07-10 22:53:06 +0300

    m9 end, study teleport, fragment

---
+++ 09_Diving_Deeper_into_Components/src/components/ErrorAlert.vue
@@ -0,0 +1,18 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+<style scoped>
+dialog {
+  position: fixed;
+  margin: 0;
+  top: 20vh;
+  left: 30%;
+  width: 40%;
+  padding: 1rem;
+  border-radius: 1rem;
+  background: rgb(255, 255, 255);
+  box-shadow: 0 2px 10px black;
+}
+</style>
--- 09_Diving_Deeper_into_Components/src/components/ManageGoals.vue
+++ 09_Diving_Deeper_into_Components/src/components/ManageGoals.vue
@@ -1,6 +1,37 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input />
-  </div>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <teleport to="body">
+    <ErrorAlert v-if="inputIsInvalid">
+      <h2>input must no empty</h2>
+      <button @click="confirmError">Okay</button>
+    </ErrorAlert>
+  </teleport>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+export default {
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredGoal = this.$refs.goal.value;
+      console.log(enteredGoal);
+      if (enteredGoal === '') {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+  components: {
+    ErrorAlert,
+  },
+};
+</script>

9185462f236f36c6aa6b44265f2aea9045f358c8
unknown <seledkovti@gmail.com>
2022-07-10 13:41:57 +0300

    v110 global and local import components

---
+++ 09_Diving_Deeper_into_Components/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- work any syntax upperCamelCase or kebab-case -->
+    <TheHeader />
+    <the-header />
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+export default {
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  components: {
+    TheHeader: TheHeader,
+    BadgeList,
+    UserInfo,
+    // 'the-header': TheHeader,
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>

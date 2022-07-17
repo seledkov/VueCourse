31bcf97c427402fd6a82c9b4e750d32bc5b05ff5
unknown <seledkovti@gmail.com>
2022-07-17 21:12:45 +0300

    019 basic composition api

---
+++ 19_The_Composition_Api/src/components/UserData.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>{{ userName }}</h2>
+  <h3>{{ age }}</h3>
+</template>
+
+<script>
+import { computed, inject } from 'vue';
+export default {
+  props: ['firstName', 'lastName'],
+
+  setup(props, context) {
+    const userName = computed(() => props.firstName + ' ' + props.lastName);
+
+    console.log(context);
+
+    const age = inject('keyName');
+    return { userName, age };
+  },
+  // computed: {
+  //   userName() {
+  //     return this.firstName + ' ' + this.lastName;
+  //   },
+  // },
+};
+</script>
